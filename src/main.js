b4f137f6189a6fd660e8415b1964925e54e5e569
Dhemler Omapas <omapasdhemz77@gmail.com>
2023-08-15 22:47:08 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/section.css';
+import '../styles/components/section2.css';
+import '../styles/components/section3.css';
+import '../styles/utils.css';
+
